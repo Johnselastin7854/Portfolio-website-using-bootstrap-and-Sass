199f00fa7f689ff3c80e7e477d555705a635adb8
selasinmathew <77019516+Johnselastin7854@users.noreply.github.com>
2023-03-02 00:44:24 -0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// Glighbox for external video popup player
+const lightbox = GLightbox({
+    'touchNavigation': true,
+    'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    'autoPlayVideos': 'true',
+});
